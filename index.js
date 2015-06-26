4ba88dab71f84846215859385762d28042482eb5
Thomas Wrenn <twrenn@creativeworx.com>
2015-06-25 23:10:36 -0700

    Initial
    Signed-off-by: Thomas Wrenn <twrenn@creativeworx.com>

---
+++ index.js
@@ -0,0 +1,37 @@
+var app = require('http').createServer(handler);
+var statusCode = 200;
+
+app.listen(9021);
+
+function handler (req, res) {
+	var data = '';
+
+	console.log(req.method);
+
+	if (req.method == "POST") {
+		req.on('data', function(chunk) {
+			data += chunk;
+		});
+
+		req.on('end', function() {
+			console.log('Received POST body data:');
+			console.log(data.toString());
+		});
+	}
+	if (req.method == "GET") {
+		req.on('data', function(chunk) {
+			data += chunk;
+		});
+
+		req.on('end', function() {
+			console.log('Received GET body data:');
+			console.log(data.toString());
+		});
+	}
+
+	res.writeHead(statusCode, {'Content-Type': 'text/plain'});
+	res.end();
+}
+
+console.log("Listening to port 9021");
+console.log("Returning status code " + statusCode.toString());
